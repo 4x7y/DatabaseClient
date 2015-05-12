a465ba84ed47b1b6f3bac635eaccb9ebb98db1d6
Yuechuan Xue <xueyuechuan@gmail.com>
2015-05-12 10:38:36 +0800

    First Commit
    Tutorial:
    http://www.java-tutorial.ch/core-java-tutorial/mysql-with-java-and-maven
    -tutorial

---
+++ database/src/main/java/me/yuechuan/database/App.java
@@ -0,0 +1,15 @@
+package me.yuechuan.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
